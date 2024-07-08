22322cf03dc7b7feaeacf1136c75e0fa2f829337
hihotz <hsb5513@naver.com>
2024-07-08 12:54:08 +0900

    체크박스 기능 추가
    자동 로그인 기능 추가

--- blueapp/Views/Splash/LoginPage.xaml.cs
+++ blueapp/Views/Splash/LoginPage.xaml.cs
@@ -21,15 +21,28 @@ public partial class LoginPage : ContentPage
     #region 체크박스 유무에 따른 동작
     private async void LoadSavedUserName()
     {
-        if (SaveUserNameCheck.IsChecked)
+        // 자동 로그인 체크 시 id, pw 저장
+        if (AutoLoginCheck.IsChecked)
         {
             try
             {
-                var savedUserName = await SecureStorage.GetAsync("UserName");
-                if (savedUserName != null)
-                {
-                    UsernameEntry.Text = savedUserName;
-                }
+                // 저장된 id, pw 불러오기
+                UsernameEntry.Text = await SecureStorage.GetAsync("UserName");
+                PasswordEntry.Text = await SecureStorage.GetAsync("UserPW");
+
+                await Login();
+            }
+            catch (Exception ex)
+            {
+                // SecureStorage 예외 처리 (예: 권한 문제)
+                await DisplayAlert(AppResources.error, AppResources.error + " : " + ex.Message, AppResources.ok);
+            }
+        }
+        else if (SaveUserNameCheck.IsChecked)
+        {
+            try
+            {
+                UsernameEntry.Text = await SecureStorage.GetAsync("UserName");
             }
             catch (Exception ex)
             {
@@ -50,45 +63,85 @@ public partial class LoginPage : ContentPage
             }
         }
     }
+
+    private async Task CheckBox_Check(string username, string password)
+    {
+        // 자동 로그인 체크 시 id, pw 저장
+        if (AutoLoginCheck.IsChecked)
+        {
+            try
+            {
+                await SecureStorage.SetAsync("UserName", username);
+                await SecureStorage.SetAsync("UserPW", password);
+            }
+            catch (Exception ex)
+            {
+                // SecureStorage 예외 처리 (예: 권한 문제)
+                await DisplayAlert(AppResources.error, AppResources.error + " : " + ex.Message, AppResources.ok);
+            }
+        }
+        // 유저 id 저장 체크시 id만 저장
+        else if (SaveUserNameCheck.IsChecked)
+        {
+            try
+            {
+                await SecureStorage.SetAsync("UserName", username);
+            }
+            catch (Exception ex)
+            {
+                // SecureStorage 예외 처리 (예: 권한 문제)
+                await DisplayAlert(AppResources.error, AppResources.error + " : " + ex.Message, AppResources.ok);
+            }
+        }
+    }
     #endregion
 
     #region 로그인
     private async void OnLoginClicked(object sender, EventArgs e)
     {
-        string username = UsernameEntry.Text;
-        string password = PasswordEntry.Text;
-
-        ApiResponse apiResponse = await _loginviewmodel.LoginAsync(username, password);
-
-        if (apiResponse.StatusCode == 200)
+        try
         {
-            // 체크박스에 따른 로그인 정보 저장
-            if (SaveUserNameCheck.IsChecked)
+            await Login();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert(AppResources.error, AppResources.error + " : " + ex.Message, AppResources.ok);
+        }
+    }
+
+    private async Task Login()
+    {
+        try
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            ApiResponse apiResponse = await _loginviewmodel.LoginAsync(username, password);
+
+            // 로그인 성공시
+            if (apiResponse.StatusCode == 200)
             {
-                try
+                // 로그인 성공시에만 체크박스 기능 동작
+                await CheckBox_Check(username, password);
+
+                // 페이지 전환 이벤트
+                if (Application.Current != null)
                 {
-                    await SecureStorage.SetAsync("UserName", username);
-                }
-                catch (Exception ex)
-                {
-                    // SecureStorage 예외 처리 (예: 권한 문제)
-                    await DisplayAlert(AppResources.error, AppResources.error + " : " + ex.Message, AppResources.ok);
+                    var appShell = new AppShell();
+                    await this.FadeTo(0, 100);
+                    Application.Current.MainPage = appShell;
+                    await appShell.FadeTo(1, 100);
                 }
             }
-
-            // 페이지 전환 이벤트
-            if (Application.Current != null)
+            else
             {
-                var appShell = new AppShell();
-                await this.FadeTo(0, 100);
-                Application.Current.MainPage = appShell;
-                await appShell.FadeTo(1, 100);
+                // 로그인 실패시 DisplayAlert로 사용자에게 알림 표시 : 로그인 실패, 에러메시지, 확인
+                await DisplayAlert(AppResources.login + " " + AppResources.failed, apiResponse.Message, AppResources.ok);
             }
         }
-        else
+        catch (Exception ex)
         {
-            // 로그인 실패시 DisplayAlert로 사용자에게 알림 표시 : 로그인 실패, 에러메시지, 확인
-            await DisplayAlert(AppResources.login + " " + AppResources.failed, apiResponse.Message, AppResources.ok);
+            await DisplayAlert(AppResources.error, AppResources.error + " : " + ex.Message, AppResources.ok);
         }
     }
     #endregion
